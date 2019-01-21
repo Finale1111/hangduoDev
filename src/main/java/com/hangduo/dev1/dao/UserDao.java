dbe058d7749ca2ce890f78bb0965cb62e0f7cf60
hezilong <hezilong1111@126.com>
2019-01-21 09:56:43 +0800

    add method--hzl

--- src/main/java/com/hangduo/dev1/dao/UserDao.java
+++ src/main/java/com/hangduo/dev1/dao/UserDao.java
@@ -2,8 +2,14 @@ package com.hangduo.dev1.dao;
 
 import com.hangduo.dev1.entity.User;
 
+import java.util.List;
+
 public interface UserDao {
 
     User getUserByUid(int uid);
+
+    List<User> getUsers(int index);
+
+    //这是刚刚写的
 
 }
